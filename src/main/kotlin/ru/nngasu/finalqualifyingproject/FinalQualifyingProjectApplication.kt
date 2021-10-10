5202650ae5312565e90df442114d70ab551bf965
Excome <02maxsim06@gmail.com>
2021-10-10 23:03:13 +0300

    Spring initialize commit

---
+++ src/main/kotlin/ru/nngasu/finalqualifyingproject/FinalQualifyingProjectApplication.kt
@@ -0,0 +1,11 @@
+package ru.nngasu.finalqualifyingproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinalQualifyingProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinalQualifyingProjectApplication>(*args)
+}
---
+++ src/test/kotlin/ru/nngasu/finalqualifyingproject/FinalQualifyingProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.nngasu.finalqualifyingproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinalQualifyingProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
