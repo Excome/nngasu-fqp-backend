9f0ae37128bad9b834b30e86c6e761d4530230e2
Excome <02maxsim06@gmail.com>
2022-02-18 23:47:59 +0300

    add json view formatter for user

--- src/main/kotlin/ru/nngasu/finalqualifyingproject/model/Role.kt
+++ src/main/kotlin/ru/nngasu/finalqualifyingproject/model/Role.kt
@@ -1,6 +1,8 @@
 package ru.nngasu.finalqualifyingproject.model
 
+import com.fasterxml.jackson.annotation.JsonView
 import org.springframework.security.core.GrantedAuthority
+import ru.nngasu.finalqualifyingproject.model.jsonView.RoleView
 
 /**
 @author Peshekhonov, Maksim
@@ -9,11 +11,17 @@ enum class Role(val id: Int, private val priority: Int): GrantedAuthority {
     /**
     @see MAX priority can be 1000!
     */
+    @JsonView(RoleView.Name::class)
     ROLE_GUEST(0, 100),
+    @JsonView(RoleView.Name::class)
     ROLE_USER(1, 200),
+    @JsonView(RoleView.Name::class)
     ROLE_TEACHER(2, 400),
+    @JsonView(RoleView.Name::class)
     ROLE_TECHNICIAN(3, 500),
+    @JsonView(RoleView.Name::class)
     ROLE_MODERATOR(4, 700),
+    @JsonView(RoleView.Name::class)
     ROLE_ADMIN(5, 1000);
 
     override fun getAuthority(): String {
---
+++ src/main/kotlin/ru/nngasu/finalqualifyingproject/model/jsonView/RoleView.kt
@@ -0,0 +1,9 @@
+package ru.nngasu.finalqualifyingproject.model.jsonView
+
+/**
+ * This marker interfaces for set serialization view of Role to Json
+ * @author Peshekhonov Maksim
+ */
+class RoleView {
+    interface Name: ErrorView.Info
+}
